5183945a71b210c9bfc08693f1e6a70b9666e148
marymatamedina <marymatadeChalamet@gmail.com>
2026-03-31 20:02:38 -0400

    tasks list done

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Task
 
 # Create your views here.
+class TaskListView(ListView):
+    template_name = 'task_list.html'
+    model = Task
