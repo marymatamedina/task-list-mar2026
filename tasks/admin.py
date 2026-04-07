dfda828a755bef04bd8981d041e39d8a492ed106
marymatamedina <marymatadeChalamet@gmail.com>
2026-04-07 19:00:39 -0400

    admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Task
 
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
+    list_filter = ('created_at', 'updated_at')
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
